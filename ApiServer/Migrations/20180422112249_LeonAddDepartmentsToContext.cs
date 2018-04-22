d8d8f747b4b18eacc953f6b8e6d4650b3da0df0a
leon <leon@damaozhu.com>
2018-04-22 19:25:21 +0800

    添加Departments To ApiContext

---
+++ ApiServer/Migrations/20180422112249_LeonAddDepartmentsToContext.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ApiServer.Migrations
+{
+    public partial class LeonAddDepartmentsToContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Department_DepartmentId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Organizations_OrganizationId",
+                table: "Department");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Department_Department_ParentId",
+                table: "Department");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrganMember_Department_DepartmentId",
+                table: "OrganMember");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Department",
+                table: "Department");
+
+            migrationBuilder.RenameTable(
+                name: "Department",
+                newName: "Departments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Department_ParentId",
+                table: "Departments",
+                newName: "IX_Departments_ParentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Department_OrganizationId",
+                table: "Departments",
+                newName: "IX_Departments_OrganizationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Departments_DepartmentId",
+                table: "Accounts",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Organizations_OrganizationId",
+                table: "Departments",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_Departments_ParentId",
+                table: "Departments",
+                column: "ParentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrganMember_Departments_DepartmentId",
+                table: "OrganMember",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Departments_DepartmentId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Organizations_OrganizationId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Departments_Departments_ParentId",
+                table: "Departments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrganMember_Departments_DepartmentId",
+                table: "OrganMember");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments");
+
+            migrationBuilder.RenameTable(
+                name: "Departments",
+                newName: "Department");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Departments_ParentId",
+                table: "Department",
+                newName: "IX_Department_ParentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Departments_OrganizationId",
+                table: "Department",
+                newName: "IX_Department_OrganizationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Department",
+                table: "Department",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Department_DepartmentId",
+                table: "Accounts",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Organizations_OrganizationId",
+                table: "Department",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Department_Department_ParentId",
+                table: "Department",
+                column: "ParentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrganMember_Department_DepartmentId",
+                table: "OrganMember",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
