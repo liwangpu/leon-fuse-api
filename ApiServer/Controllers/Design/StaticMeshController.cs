3d4beeaaac68044b5cbb2c59abe4431db2e71655
leon <leon@damaozhu.com>
2018-04-28 19:09:45 +0800

    修改static mesh使用store仓库

--- ApiServer/Controllers/Design/StaticMeshController.cs
+++ ApiServer/Controllers/Design/StaticMeshController.cs
@@ -2,9 +2,12 @@
 using ApiServer.Data;
 using ApiServer.Models;
 using ApiServer.Services;
+using ApiServer.Stores;
 using BambooCore;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ApiServer.Controllers.Design
@@ -15,79 +18,128 @@ namespace ApiServer.Controllers.Design
     {
         private readonly ApiDbContext _ApiContext;
         private readonly Repository<StaticMesh> repo;
-
+        private readonly StaticMeshStore _StaticMeshStore;
         public StaticMeshController(ApiDbContext context)
         {
             repo = new Repository<StaticMesh>(context);
             _ApiContext = context;
+            _StaticMeshStore = new StaticMeshStore(context);
         }
 
+        #region Get 根据分页查询信息获取模型概要信息
+        /// <summary>
+        /// 根据分页查询信息获取模型概要信息
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="page"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="desc"></param>
+        /// <returns></returns>
         [HttpGet]
-        public async Task<PagedData> Get(string search, int page, int pageSize, string orderBy, bool desc)
+        [ProducesResponseType(typeof(PagedData1<StaticMeshDTO>), 200)]
+        [ProducesResponseType(typeof(PagedData1<StaticMeshDTO>), 400)]
+        public async Task<PagedData1<StaticMeshDTO>> Get(string search, int page, int pageSize, string orderBy, bool desc)
         {
-            PagingMan.CheckParam(ref search, ref page, ref pageSize);
-            return await repo.GetAsync(AuthMan.GetAccountId(this), page, pageSize, orderBy, desc,
-                d => d.Id.Contains(search) || d.Name.Contains(search) || d.Description.Contains(search));
+            var accid = AuthMan.GetAccountId(this);
+            return await _StaticMeshStore.SimpleQueryAsync(accid, page, pageSize, orderBy, desc, d => d.Id.Contains(search) || d.Name.Contains(search) || d.Description.Contains(search));
         }
+        #endregion
 
-
+        #region Get 根据id获取模型信息
+        /// <summary>
+        /// 根据id获取模型信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
-        [Produces(typeof(StaticMesh))]
+        [ProducesResponseType(typeof(StaticMeshDTO), 200)]
+        [ProducesResponseType(typeof(List<string>), 404)]
         public async Task<IActionResult> Get(string id)
         {
-            var res = await repo.GetAsync(AuthMan.GetAccountId(this), id);
-            if (res == null)
-                return NotFound();
-            repo.Context.Entry(res).Collection(d => d.Materials).Load();
-            if (res.Materials != null && res.Materials.Count > 0)
-            {
-                for (int idx = res.Materials.Count - 1; idx >= 0; idx--)
-                {
-                    var mate = res.Materials[idx];
-                    mate.FileAsset = await _ApiContext.Files.FindAsync(mate.FileAssetId);
-                }
-            }
-            return Ok(res.ToDictionary());//return Forbid();
+            var accid = AuthMan.GetAccountId(this);
+            var msg = await _StaticMeshStore.CanRead(accid, id);
+            if (msg.Count > 0)
+                return NotFound(msg);
+            var data = await _StaticMeshStore.GetByIdAsync(id);
+            return Ok(data);
         }
+        #endregion
 
-
+        #region Post 创建模型信息
+        /// <summary>
+        /// 创建模型信息
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         [HttpPost]
-        [Produces(typeof(StaticMesh))]
+        [ProducesResponseType(typeof(StaticMeshDTO), 200)]
+        [ProducesResponseType(typeof(List<string>), 400)]
         public async Task<IActionResult> Post([FromBody]StaticMeshEditModel value)
         {
-            //if (ModelState.IsValid == false)
-            //    return BadRequest(ModelState);
-            //if (string.IsNullOrEmpty(value.ProductSpecId) == false)
-            //    value.ProductSpec = await repo.Context.Set<ProductSpec>().FindAsync(value.ProductSpecId);
-            //value = await repo.CreateAsync(AuthMan.GetAccountId(this), value);
-
-
             if (ModelState.IsValid == false)
                 return BadRequest(ModelState);
-            var res = await repo.CreateAsync(AuthMan.GetAccountId(this), value.ToEntity());
-            return CreatedAtAction("Get", res);
+            var entity = new StaticMesh();
+            entity.Name = value.Name;
+            entity.Description = value.Description;
+            entity.FileAssetId = value.FileAssetId;
+            entity.Icon = value.Icon;
+            var accid = AuthMan.GetAccountId(this);
+            var msg = await _StaticMeshStore.CanCreate(accid, entity);
+            if (msg.Count > 0)
+                return BadRequest(msg);
+            await _StaticMeshStore.SaveOrUpdateAsync(accid, entity);
+            return Ok(entity.ToDTO());
         }
+        #endregion
 
+        #region Put 更新模型信息
+        /// <summary>
+        /// 更新模型信息
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         [HttpPut]
-        [Produces(typeof(StaticMesh))]
-        public async Task<IActionResult> Put([FromBody]StaticMesh value)
+        [ProducesResponseType(typeof(StaticMeshDTO), 200)]
+        [ProducesResponseType(typeof(List<string>), 400)]
+        public async Task<IActionResult> Put([FromBody]StaticMeshEditModel value)
         {
             if (ModelState.IsValid == false)
                 return BadRequest(ModelState);
-
-            var res = await repo.UpdateAsync(AuthMan.GetAccountId(this), value);
-            if (res == null)
-                return NotFound();
-            return Ok(value);
+            var accid = AuthMan.GetAccountId(this);
+            var mesh = await _StaticMeshStore._GetByIdAsync(value.Id);
+            if (mesh == null)
+                return BadRequest(new List<string>() { ValidityMessage.V_NotDataOrPermissionMsg });
+            mesh.Name = value.Name;
+            mesh.Name = value.Name;
+            mesh.Description = value.Description;
+            mesh.ModifiedTime = new DateTime();
+            var msg = await _StaticMeshStore.CanUpdate(accid, mesh);
+            if (msg.Count > 0)
+                return BadRequest(msg);
+            await _StaticMeshStore.SaveOrUpdateAsync(accid, mesh);
+            return Ok(mesh.ToDTO());
         }
+        #endregion
 
+        #region Delete 删除模型信息
+        /// <summary>
+        /// 删除模型信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(Nullable), 200)]
+        [ProducesResponseType(typeof(List<string>), 404)]
         public async Task<IActionResult> Delete(string id)
         {
-            bool bOk = await repo.DeleteAsync(AuthMan.GetAccountId(this), id);
-            if (bOk)
-                return Ok();
-            return NotFound();//return Forbid();
-        }
+            var accid = AuthMan.GetAccountId(this);
+            var msg = await _StaticMeshStore.CanDelete(accid, id);
+            if (msg.Count > 0)
+                return NotFound(msg);
+            await _StaticMeshStore.DeleteAsync(accid, id);
+            return Ok();
+        } 
+        #endregion
     }
 }
--- ApiServer/Stores/StaticMeshStore.cs
+++ ApiServer/Stores/StaticMeshStore.cs
@@ -1,7 +1,9 @@
 ﻿using ApiModel.Entities;
 using ApiServer.Data;
+using BambooCommon;
 using BambooCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
@@ -11,12 +13,84 @@ namespace ApiServer.Stores
     /// <summary>
     /// StaticMesh Store
     /// </summary>
-    public class StaticMeshStore : StoreBase<StaticMesh, StaticMeshDTO>
+    public class StaticMeshStore : StoreBase<StaticMesh, StaticMeshDTO>, IStore<StaticMesh>
     {
         #region 构造函数
         public StaticMeshStore(ApiDbContext context)
         : base(context)
         { }
+        #endregion
+
+        #region CanCreate 判断模型文件信息是否符合存储规范
+        /// <summary>
+        /// 判断模型文件信息是否符合存储规范
+        /// </summary>
+        /// <param name="accid"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task<List<string>> CanCreate(string accid, StaticMesh data)
+        {
+            var errors = new List<string>();
+            var valid = _CanSave(accid, data);
+            if (string.IsNullOrWhiteSpace(data.Name) || data.Name.Length > 50)
+                errors.Add(string.Format(ValidityMessage.V_StringLengthRejectMsg, "模型名称", 50));
+            if (string.IsNullOrWhiteSpace(data.FileAssetId))
+                errors.Add(string.Format(ValidityMessage.V_RequiredRejectMsg, "关联文件"));
+
+            return errors;
+        }
+        #endregion
+
+        #region CanUpdate 判断模型文件信息是否符合更新规范
+        /// <summary>
+        /// 判断模型文件信息是否符合更新规范
+        /// </summary>
+        /// <param name="accid"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task<List<string>> CanUpdate(string accid, StaticMesh data)
+        {
+            var errors = new List<string>();
+            var valid = _CanSave(accid, data);
+            if (string.IsNullOrWhiteSpace(data.Name) || data.Name.Length > 50)
+                errors.Add(string.Format(ValidityMessage.V_StringLengthRejectMsg, "模型名称", 50));
+            if (string.IsNullOrWhiteSpace(data.FileAssetId))
+                errors.Add(string.Format(ValidityMessage.V_RequiredRejectMsg, "关联文件"));
+
+            return errors;
+        }
+        #endregion
+
+        #region CanDelete 判断模型信息信息是否符合删除规范
+        /// <summary>
+        /// 判断模型信息信息是否符合删除规范
+        /// </summary>
+        /// <param name="accid"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<List<string>> CanDelete(string accid, string id)
+        {
+            var errors = new List<string>();
+
+            var valid = _CanDelete(accid, id);
+            if (valid.Count > 0)
+                return valid;
+            return errors;
+        }
+        #endregion
+
+        #region CanRead 判断用户是否有读取权限
+        /// <summary>
+        /// 判断用户是否有读取权限
+        /// </summary>
+        /// <param name="accid"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<List<string>> CanRead(string accid, string id)
+        {
+            var errors = new List<string>();
+            return errors;
+        } 
         #endregion
 
         #region SimpleQueryAsync 简单返回分页查询DTO信息
@@ -38,6 +112,31 @@ namespace ApiServer.Stores
         }
         #endregion
 
+        #region SaveOrUpdateAsync 更新模型信息
+        /// <summary>
+        /// 更新模型信息
+        /// </summary>
+        /// <param name="accid"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task SaveOrUpdateAsync(string accid, StaticMesh data)
+        {
+            try
+            {
+                if (!data.IsPersistence())
+                {
+                    await _DbContext.Set<StaticMesh>().AddAsync(data);
+                    //await _Repo.Context.Set<PermissionItem>().AddAsync(Permission.NewItem(accid, data.Id, "Product", PermissionType.All));
+                }
+                await _DbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("SaveOrUpdateAsync", ex);
+            }
+        } 
+        #endregion
+
         #region GetByIdAsync 根据Id返回实体DTO数据信息
         /// <summary>
         /// 根据Id返回实体DTO数据信息
@@ -49,6 +148,36 @@ namespace ApiServer.Stores
         {
             var data = await _GetByIdAsync(id);
             return data.ToDTO();
+        }
+
+
+        #endregion
+
+        #region DeleteAsync 删除产品信息
+        /// <summary>
+        /// 删除产品信息
+        /// </summary>
+        /// <param name="accid"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task DeleteAsync(string accid, string id)
+        {
+            try
+            {
+                var data = await _GetByIdAsync(id);
+                if (data.IsPersistence())
+                {
+                    //var ps = _Repo.Context.Set<PermissionItem>().Where(x => x.ResId == data.Id).FirstOrDefault();
+                    _DbContext.Set<StaticMesh>().Remove(data);
+                    //if (ps != null)
+                    //    _Repo.Context.Set<PermissionItem>().Remove(ps);
+                    await _DbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("DeleteAsync", ex);
+            }
         }
         #endregion
     }
