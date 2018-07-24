d510a15c38dc50307a4a60af4bb5abe9034abf56
leon <leon@damaozhu.com>
2018-07-24 19:07:15 +0800

    添加ui designer 做测试

---
+++ ApiServer/Controllers/UIDesigner/BSModelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers.UIDesigner
+{
+    [Authorize]
+    [Route("/[controller]")]
+    public class BSModelController : Controller
+    {
+        [Route("List")]
+        [HttpGet]
+        public async Task<IActionResult> List(string modelName)
+        {
+            var model = new
+            {
+                Resource = modelName,
+                ModelType = "List",
+                Icon = "map",
+                DisplayModel = new List<string>() { "List" },
+                Fields = new List<string>() { "Id", "Name", "Description" }
+
+            };
+            return Ok(model);
+        }
+    }
+}
