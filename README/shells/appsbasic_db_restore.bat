64037de9831e1d3a045707fca08a6dea416e31f8
leon <liwang.pu@gmail.com>
2019-04-25 18:43:12 +0800

    更改元皿和挪亚家的部署信息

--- README/shells/appsbasic_db_restore.bat
+++ README/shells/appsbasic_db_restore.bat
@@ -1,6 +1,6 @@
 @echo off
 echo start copying db json files to container
-docker cp D:/Leon/AppCode/bamboo/morejee-api/apps-basic/Apps.Basic.Service/wwwroot/AppBackup/. apps-legacy-basic-service-ym-c:/app/wwwroot/AppBackup
+docker cp D:/Leon/AppCode/bamboo/morejee-api/apps-basic/Apps.Basic.Service/wwwroot/AppBackup/. app-microservice-basic-api:/app/wwwroot/AppBackup
 echo restart docker 
 docker restart apps-legacy-basic-service-ym-c
 echo all done!
